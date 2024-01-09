a7d594cfbeab04ecf941f6e787f9fa0ee8b8f81b
tahara1102 <bururazisugita@icloud.com>
2024-01-09 17:01:11 +0900

    課題提出

--- src/main/java/jp/co/amgakuin/javaclass2023/sample20240109/SampleThread.java
+++ src/main/java/jp/co/amgakuin/javaclass2023/sample20240109/SampleThread.java
@@ -2,8 +2,12 @@ package jp.co.amgakuin.javaclass2023.sample20240109;
 
 public class SampleThread extends Thread {
 
-	public void main(String argv[]) {
+	public static void main(String argv[]) {
 		new Thread(new SampleThread()).start();
+		
+		for(int i=0; i<100; i++) {
+			System.out.println("main"+i);
+		}
 	}
 	
 	@Override
